f6f865cd5936efd2dd557ff75f71beafd5337e75
primeart <95005486+primeart@users.noreply.github.com>
2022-01-20 00:31:44 +0000

    Add files via upload

--- spa.js
+++ spa.js
@@ -84,8 +84,8 @@ function timeNow(){
 //function spa_putFileRequest(url, data, callback){
 //		httpRequest(data.imagePutUrl, 'PUT',data) //, callback=waitResponce
 //}
-window.spa_apiRequestCallbacks={}
-window.spa_apiRequestQueue=[]
+window.spa_apiRequestCallbacks={}  //executed, awaiting responce
+window.spa_apiRequestQueue=[]   //to be executed; not yet executed
 
 //blocking == other commands will not be executed (and insteant sent to queue) until this command got responce; async otherwice
 //unique == only one command with this command name can be executing (awaiting result) at time; other calls for same command will be ignored until responce recieved
@@ -93,29 +93,35 @@ function spa_apiRequest(commandName, data, callback, blocking, unique){
 	if (!window.spa_requestUrl){
 		return
 	}
+	if (blocking==undefined){blocking=true}
+	if (unique==undefined){unique=false}
 	console.log('spa_apiRequest :: apiCommand=', commandName)
 	//document.getElementById('load_screen_root').innerHTML += '<br /><br />spa_apiRequest:: apiCommand='+commandName
 	//ui_waiter(true)
 
-	if (unique){
-		for(var i=0;i < spa_apiRequestCallbacks.length;i++) {
-			if (spa_apiRequestCallbacks[i][0] == commandName){ //is blocking
-				console.log('command '+commandName+' rejected. it was declaread as unique, and this is repeating call while no responce recieved yet')
-				return true
-			}
+	for(var i=0;i < spa_apiRequestCallbacks.length;i++) {
+		if (unique && spa_apiRequestCallbacks[i][0] == commandName){
+			console.log('command '+commandName+' rejected. it was declaread as unique, and this is repeating call while no responce recieved yet')
+			return true
 		}
-	}
-
-	for(var i=0;i < spa_apiRequestQueue.length;i++) {
-		if (spa_apiRequestQueue[i][3] == true){ //is blocking
-			window.spa_apiRequestQueue.push([commandName, data, callback, blocking?true:false, unique?true:false])
+		if (spa_apiRequestCallbacks[i][3] == true){
+			console.log('command "'+commandName+'" not executed but added to queue because we awaiting responce for blocking request "'+spa_apiRequestCallbacks[i][0] +'"')
+			window.spa_apiRequestQueue.push([commandName, data, callback, blocking, unique])
 			return true
 		}
 	}
 
+
+//	for(var i=0;i < spa_apiRequestQueue.length;i++) {
+//		if (spa_apiRequestQueue[i][3] == true){ //is blocking
+//			window.spa_apiRequestQueue.push([commandName, data, callback, blocking, unique])
+//			return true
+//		}
+//	}
+
 	spa_requestId = timeNow()
 
-	window.spa_apiRequestCallbacks[spa_requestId]=[commandName, data, callback, blocking?true:false, unique?true:false]
+	window.spa_apiRequestCallbacks[spa_requestId]=[commandName, data, callback, blocking, unique]
 	window.spa_responceAwaitTries=0
 
 	if (!data.type){ //not a file
@@ -143,6 +149,7 @@ function spa_addResponceScript(spa_requestId) {
 		//spa_requestId = this
 		if (callback=window.spa_apiRequestCallbacks[spa_requestId]){
 				console.log("callback found, calling");
+				delete window.spa_apiRequestCallbacks[spa_requestId]
 				callback[2](spa_responces[spa_requestId].responceData, callback[1])// !== false && ()
 		}
 		if (window.spa_apiRequestQueue.length>0){
